e78c604872228660d055b8f614881f917c0171f9
VictorGillioz <v.a.gillioz@gmail.com>
2023-05-12 17:51:42 +0200

    pylint error correction

--- utils/data_loading.py
+++ utils/data_loading.py
@@ -7,7 +7,8 @@ from typing import Tuple
 
 # TODO(victor): modify the loader once a data format has been decided
 
-def load_mesh_from_obj(obj_path: str, verbose: bool = False) -> Tuple[np.ndarray, np.ndarray]:
+def load_mesh_from_obj(obj_path: str,
+                       verbose: bool = False) -> Tuple[np.ndarray, np.ndarray]:
 
     mesh = meshio.read(obj_path)
 
@@ -15,8 +16,8 @@ def load_mesh_from_obj(obj_path: str, verbose: bool = False) -> Tuple[np.ndarray
     cells = mesh.cells_dict['triangle']
 
     if verbose:
-        logging.debug('Shape of nodes :', nodes.shape)
-        logging.debug('Shape of cells :', cells.shape)
+        logging.info(f'Shape of nodes : {nodes.shape}')
+        logging.info(f'Shape of cells : {nodes.shape}')
 
     return nodes, cells
 
