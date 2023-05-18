276a66d56e97c85b04d9030535b2c01448bae814
VictorGillioz <v.a.gillioz@gmail.com>
2023-05-18 12:32:23 +0200

    docstring, parameter name and typing changes

--- meshnets/model/model.py
+++ meshnets/model/model.py
@@ -1,6 +1,7 @@
 """Define the main MeshGraphNet model"""
 
-import numpy as np
+from typing import List
+
 import torch
 
 from meshnets.datatypes.graph import Graph
@@ -12,26 +13,27 @@ from meshnets.model.processor import GraphProcessor
 class MeshGraphNet(torch.nn.Module):
     """MeshGraphNet model.
     
-    It is composed of a GraphEncoder, a GraphProcessor and a GraphDecoder.
-    It takes as input Graph objects, encodes their node and mesh features
-    to a latent representation, apply message passing steps
-    on the latent Graph objects, and decodes the output features
-    at each node."""
+    The model architecture is composed of a GraphEncoder, a GraphProcessor
+    and a GraphDecoder, and takes as input a list of Graph objects. Each Graph
+    object's features are encoded to form a latent Graph representation. 
+    Message passing steps are then applied by the Processor on the latent Graph
+    object and the Decoder outputs features at each node from the processed
+    latent node features."""
 
-    def __init__(self, node_feats_size, mesh_feats_size, output_size,
+    def __init__(self, node_features_size, mesh_features_size, output_size,
                  latent_size, num_mlp_layers, message_passing_steps):
         """Initialize the GraphEncoder, GraphProcessor and GraphDecoder
         of the MeshGraphNet."""
         super().__init__()
 
-        self.encoder = GraphEncoder(node_feats_size, mesh_feats_size,
+        self.encoder = GraphEncoder(node_features_size, mesh_features_size,
                                     latent_size, num_mlp_layers)
 
         self.processor = GraphProcessor(latent_size, message_passing_steps)
 
         self.decoder = GraphDecoder(output_size, latent_size, num_mlp_layers)
 
-    def forward(self, graph_list: np.ndarray[Graph]) -> torch.Tensor:
+    def forward(self, graph_list: List[Graph]) -> List[torch.Tensor]:
         """Sequentially apply the MeshGraphNet to each Graph object
         in the input list.
         
@@ -39,11 +41,11 @@ class MeshGraphNet(torch.nn.Module):
 
         # TODO(victor): normalize data
 
-        output = []
+        output_list = []
         for graph in graph_list:
             latent_graph = self.encoder(graph)
             latent_graph = self.processor(latent_graph)
-            pred = self.decoder(latent_graph)
-            output.append(pred)
+            prediction = self.decoder(latent_graph)
+            output_list.append(prediction)
 
-        return output
+        return output_list
