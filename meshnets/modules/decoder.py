35b0b492bdbe764a22a5a32370ae44d280bbf8f6
VictorGillioz <v.a.gillioz@gmail.com>
2023-05-25 16:14:47 +0200

    Identity encoder decoder implementation

--- meshnets/modules/decoder.py
+++ meshnets/modules/decoder.py
@@ -1,10 +1,29 @@
 """"Define the GraphDecoder class."""
 
+import torch
+from torch.nn import Identity
+from torch_geometric.data import Batch
 
-class GraphDecoder:
 
-    def __init__(self, *args):
-        pass
+class GraphDecoder(torch.nn.Module):
+    """Decoder for graphs.
+    
+    Decode latent graphs to an output Tensor corresponding to predicted
+    node features by applying an MLP decoder to each node features.
+    """
 
-    def __call__(self, *args):
-        pass
+    def __init__(self, output_size, latent_size, num_mlp_layers):
+        """Initialize the node features decoder given the output size,
+        latent size and number of MLP layers."""
+        super().__init__()
+
+        # TODO(victor) : implement decoder
+        self.node_decoder = Identity(output_size, latent_size, num_mlp_layers)
+
+    def forward(self, graph: Batch) -> torch.Tensor:
+        """Decode the latent node features of a batch of graphs
+        to output node features.
+        
+        Return the output Tensor."""
+
+        return self.node_decoder(graph.node_features)
