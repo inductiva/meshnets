e1aee4f849894581ee19d980299b1cd297bfaeb6
VictorGillioz <v.a.gillioz@gmail.com>
2023-05-27 15:53:47 +0200

    improved comments and modularity

--- meshnets/utils/data_loading.py
+++ meshnets/utils/data_loading.py
@@ -5,6 +5,7 @@ from typing import Tuple
 
 from absl import logging
 import meshio
+from meshio._mesh import Mesh
 import numpy as np
 import pyvista as pv
 
@@ -19,12 +20,19 @@ def load_edge_mesh_pv(
     using pyvista."""
 
     mesh = pv.read(obj_path)
-    # `use_all_points=False` discards duplicates and renumber the nodes
-    # TODO(victor): verify that node pressure array is modified accordingly
-    # WARNING: the remeshing should be done prior on the mesh file, not here
+    # Extract all the internal/external edges of the dataset as PolyData.
+    # This produces a full wireframe representation of the input dataset.
+    # WARNING: `use_all_points=False` discards duplicates and unused nodes,
+    # but remeshing should be done prior on the mesh file, not here
+    # TODO(victor): check if True results in point renumbering
     edge_mesh = mesh.extract_all_edges(use_all_points=True)
 
     nodes = edge_mesh.points
+
+    # Returns the edges as an array with format :
+    # [2, point_id_1, point_id_2, 2, point_id_3, point_id_4, 2, ...]
+    # Reshape it to (number_of_edges, 3) and remove the first column containing
+    # only 2s
     edge_list = edge_mesh.lines.reshape((-1, 3))[:, 1:]
 
     if verbose:
@@ -32,6 +40,8 @@ def load_edge_mesh_pv(
         logging.info('Edge list shape : %s', edge_list.shape)
 
     if get_pressure:
+        # reshape from (number_of_points,) to (number_of_points, 1)
+        # to be consistent with cases of multiple node features
         # TODO(victor): decide if array reshape is necessary
         pressure = edge_mesh.get_array('p', preference='point').reshape(-1, 1)
         if verbose:
@@ -40,6 +50,31 @@ def load_edge_mesh_pv(
         return nodes, edge_list, pressure
     else:
         return nodes, edge_list
+
+
+def edges_from_meshio_mesh(mesh: Mesh) -> np.ndarray:
+    """Produce an array containing all the edges in a meshio Mesh object.
+    
+    This array can include duplicates."""
+
+    edges_list = []
+    for cell in mesh.cells:
+        cell_faces = cell.data
+        shape_vertices_nb = cell_faces.shape[1]
+
+        for i in range(shape_vertices_nb - 1):
+            edges_list.append(cell_faces[:, i:(i + 2)])
+        if shape_vertices_nb > 2:
+            # get the edge between the first and last node of the faces
+            edges_list.append(cell_faces[:, ::(shape_vertices_nb - 1)])
+
+        # a bit easier to read but runs approximately 10 times slower
+        # for i in range(shape_vertices_nb - 1):
+        #     edges_list.append(cell_faces[:, [i, i + 1]])
+        # if shape_vertices_nb > 2:
+        #     edges_list.append(cell_faces[:, [0, -1]])
+
+    return np.concatenate(edges_list, axis=0)
 
 
 def load_edge_mesh_meshio(
@@ -61,25 +96,7 @@ def load_edge_mesh_meshio(
     # TODO: this should probably be avoided at file level
     nodes = mesh.points.byteswap().newbyteorder('<')
 
-    # extract all the edges in the mesh
-    edges_list = []
-    for cell in mesh.cells:
-        cell_faces = cell.data
-        shape_vertices_nb = cell_faces.shape[1]
-
-        for i in range(shape_vertices_nb - 1):
-            edges_list.append(cell_faces[:, i:(i + 2)])
-        if shape_vertices_nb > 2:
-            # get the edge between the first and last node of the faces
-            edges_list.append(cell_faces[:, ::(shape_vertices_nb - 1)])
-
-        # a bit easier to read but runs approximately 10 times slower
-        # for i in range(shape_vertices_nb - 1):
-        #     edges_list.append(cell_faces[:, [i, i + 1]])
-        # if shape_vertices_nb > 2:
-        #     edges_list.append(cell_faces[:, [0, -1]])
-
-    edges = np.concatenate(edges_list, axis=0)
+    edges = edges_from_meshio_mesh(mesh)
 
     if verbose:
         logging.info('Nodes shape : %s', nodes.shape)
