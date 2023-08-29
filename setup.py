78baf932f7eb73420dc590ead9958c295fb74a72
augusto <augustoluisperes@gmail.com>
2023-08-29 17:35:49 +0100

    Ivan's comment

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
 """Setup script"""
 from setuptools import setup, find_packages
 
-setup(name='meshenets', version='1.0', packages=find_packages())
+setup(name='meshenets', version='0.1', packages=find_packages())
