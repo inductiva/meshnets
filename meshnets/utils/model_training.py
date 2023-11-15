dc0e225767a266d63696c49dbac835da977aa53d
augusto <augustoluisperes@gmail.com>
2023-11-15 16:52:33 +0000

    Refactor to use the hugging face interface

--- meshnets/utils/model_training.py
+++ meshnets/utils/model_training.py
@@ -5,7 +5,7 @@ or be used for tuning using Ray."""
 
 import pytorch_lightning as pl
 from pytorch_lightning.callbacks import ModelCheckpoint
-from torch_geometric.loader import DataLoader
+import torch
 
 import ray
 import ray.train.lightning
@@ -17,16 +17,34 @@ from meshnets.utils.callbacks import GPUUsage
 from meshnets.utils.callbacks import GradientNorm
 from meshnets.utils.callbacks import MLFlowLoggerFinalizeCheckpointer
 
+import meshnets
+import datasets
+
+
+def get_node_feature_size(dataloader):
+    example = next(iter(dataloader))
+    return example.x.shape[-1]
+
+
+def get_edge_feature_size(dataloader):
+    example = next(iter(dataloader))
+    return example.edge_attr.shape[-1]
+
+
+def get_output_size(dataloader):
+    example = next(iter(dataloader))
+    if example.y.ndim == 1:
+        return 1
+    return example.y.shape[-1]
+
 
 def train_model(config):
     """Train the MeshGraphNet model given the training config, experiment config
     and datasets.
     """
-    train_dataset = config['train_dataset']
-    validation_datasets = config['validation_datasets']
-    validation_datasets_names = config['validation_datasets_names']
+    dataset_version = config['dataset_version']
+    val_dataset_versions = config['val_dataset_versions']
 
-    # Config access
     # Dataloader config
     batch_size = config['batch_size']
     # Model config
@@ -48,33 +66,67 @@ def train_model(config):
     # Checkpoint config
     save_top_k = config['save_top_k']
 
-    # Compute the training dataset stats for normalization
-    train_stats = train_dataset.dataset[train_dataset.indices].get_stats()
+    # TODO(augusto): Compute the training dataset stats for
+    # normalization. Previously this was done in the dataset
+    # class. Now we need a way to iterate over the dataset to compute
+    # the stats.
 
-    # Load the training dataset
-    train_loader = DataLoader(train_dataset,
-                              batch_size=batch_size,
-                              num_workers=num_workers_loader,
-                              persistent_workers=True,
-                              shuffle=True)
+    train_dataset = datasets.load_dataset('inductiva/wind_tunnel',
+                                          version=dataset_version,
+                                          split='train')
+    train_dataset = (train_dataset.map(
+        lambda x: meshnets.data_processing.data_mappers.to_undirected(
+            x, 'edges')).map(
+                meshnets.data_processing.data_mappers.make_edge_features).map(
+                    lambda x: meshnets.data_processing.data_mappers.
+                    make_node_features(x, 'wind_vector')))
 
-    # Load the validation datasets
+    train_loader = torch.utils.data.DataLoader(
+        train_dataset,
+        collate_fn=meshnets.data_processing.torch_utils.dict_to_geometric_data,
+        batch_size=batch_size,
+        num_workers=num_workers_loader,
+        shuffle=True)
+
     validation_loaders = []
-    for val_dataset in validation_datasets:
-        validation_loaders.append(
-            DataLoader(val_dataset,
-                       batch_size=batch_size,
-                       num_workers=num_workers_loader,
-                       persistent_workers=True,
-                       shuffle=False))
+    for val_dataset_version in val_dataset_versions:
+        val_dataset = datasets.load_dataset('inductiva/wind_tunnel',
+                                            version=val_dataset_version,
+                                            split='train')
+        val_dataset = (val_dataset.map(
+            lambda x: meshnets.data_processing.data_mappers.
+            to_undirected(x, 'edges')).map(
+                meshnets.data_processing.data_mappers.make_edge_features).map(
+                    lambda x: meshnets.data_processing.data_mappers.
+                    make_node_features(x, 'wind_vector')))
 
-    train_dataset_name = validation_datasets_names[0]
+        validation_loaders.append(
+            torch.utils.data.DataLoader(val_dataset,
+                                        batch_size=batch_size,
+                                        num_workers=num_workers_loader,
+                                        shuffle=False))
+
+    node_feature_size = get_node_feature_size(train_loader)
+    edge_feature_size = get_edge_feature_size(train_loader)
+    output_size = get_output_size(train_loader)
+
+    # TODO(augusto): Priority: This must be changes. At the moment
+    # this is just for testing the hugging face integration. It will
+    # be changed with the highest priority.
+    train_stats = {
+        'x_mean': torch.tensor([0.0]),
+        'x_std': torch.tensor([1.0]),
+        'edge_attr_mean': torch.tensor([0.0]),
+        'edge_attr_std': torch.tensor([1.0]),
+        'y_mean': torch.tensor([0.0]),
+        'y_std': torch.tensor([1.])
+    }
 
     lightning_wrapper = MGNLightningWrapper(
         MeshGraphNet,
-        node_features_size=train_dataset.dataset.num_node_features,
-        edge_features_size=train_dataset.dataset.num_edge_features,
-        output_size=train_dataset.dataset.num_label_features,
+        node_features_size=node_feature_size,
+        edge_features_size=edge_feature_size,
+        output_size=output_size,
         latent_size=latent_size,
         num_mlp_layers=num_mlp_layers,
         message_passing_steps=message_passing_steps,
@@ -84,7 +136,7 @@ def train_model(config):
         edge_attr_std=train_stats['edge_attr_std'],
         y_mean=train_stats['y_mean'],
         y_std=train_stats['y_std'],
-        validation_datasets_names=validation_datasets_names,
+        validation_datasets_names=val_dataset_versions,
         learning_rate=learning_rate)
 
     # The logger creates a new MLFlow run automatically
@@ -95,15 +147,15 @@ def train_model(config):
     num_params = sum(p.numel() for p in lightning_wrapper.model.parameters())
     # Log the config parameters and training dataset stats for the run to MLFlow
     mlf_logger.log_hyperparams({
-        'train_dataset_name': train_dataset_name,
+        'train_dataset_name': dataset_version,
         'batch_size': batch_size,
         'num_params': num_params
     })
 
     callbacks = []
-    monitor_metric = f'val_loss_{train_dataset_name}'
+    monitor_metric = f'val_loss_{dataset_version}'
     # Add a suffix following lightning logging behavior
-    suffix = '' if len(validation_datasets_names) == 1 else '/dataloader_idx_0'
+    suffix = '' if len(val_dataset_versions) == 1 else '/dataloader_idx_0'
 
     # Save checkpoints locally in the mlflow folder
     checkpoint_callback = ModelCheckpoint(monitor=monitor_metric + suffix,
